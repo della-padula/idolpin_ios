ba43fdfbc5f881e591c50c0a21c7fdaa43003deb
della-padula <della.kimko@gmail.com>
2020-01-01 11:23:49 +0900

    Scene Delegate 제거

---
+++ Pods/Target Support Files/Pods-idolpin/Pods-idolpin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_idolpin : NSObject
+@end
+@implementation PodsDummy_Pods_idolpin
+@end
---
+++ idolpinTests/idolpinTests.swift
@@ -0,0 +1,33 @@
+//
+//  idolpinTests.swift
+//  idolpinTests
+//
+//  Created by TaeinKim on 2019/12/21.
+//  Copyright © 2019 TaeinKim. All rights reserved.
+//
+
+import XCTest
+
+class idolpinTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
