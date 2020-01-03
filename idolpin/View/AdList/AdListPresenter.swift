9e346e234952df2b2a68e13e903fb49a7910a42d
della-padula <della.kimko@gmail.com>
2020-01-03 15:10:30 +0900

    AdList MVP 틀 구현

---
+++ idolpin/View/AdList/AdListPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  AdListPresenter.swift
+//  idolpin
+//
+//  Created by TaeinKim on 2020/01/25.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import Foundation
+
+class AdListPresenter: AdListPresenterProtocol {
+    
+}
