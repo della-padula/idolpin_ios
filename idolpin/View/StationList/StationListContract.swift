2c24695139ea0cdecd3076a35ed7be0211505ba9
della-padula <della.kimko@gmail.com>
2020-01-04 11:02:27 +0900

    Station List MVP 구조 구현

---
+++ idolpin/View/StationList/StationListContract.swift
@@ -0,0 +1,21 @@
+//
+//  StationListContract.swift
+//  idolpin
+//
+//  Created by TaeinKim on 2020/01/25.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import Foundation
+
+protocol StationListViewProtocol {
+    
+}
+
+protocol StationListPresenterProtocol {
+    
+}
+
+protocol StationListModelPresenter {
+    
+}
---
+++ idolpin/View/StationList/StationListModel.swift
@@ -0,0 +1,13 @@
+//
+//  StationListModel.swift
+//  idolpin
+//
+//  Created by TaeinKim on 2020/01/25.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import Foundation
+
+class StationListModel: StationListModelPresenter {
+    
+}
