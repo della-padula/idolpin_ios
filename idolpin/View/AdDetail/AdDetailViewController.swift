3e93a3c21cf63032dee57c4c4a315d3fef770681
della-padula <della.kimko@gmail.com>
2020-01-05 12:10:42 +0900

    Ad Detail MVP 틀 구현

---
+++ idolpin/View/AdDetail/AdDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdListViewController.swift
+//  idolpin
+//
+//  Created by TaeinKim on 2020/01/25.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import UIKit
+
+class AdDetailViewController: UIViewController, AdDetailViewProtocol {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
