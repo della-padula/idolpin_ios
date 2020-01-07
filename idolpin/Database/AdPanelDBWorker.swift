e0f229bc12bb5a40a2d32a7d1e68076b669cdbb6
della-padula <della.kimko@gmail.com>
2020-01-07 20:30:32 +0900

    CoreData Model, Worker 생성

---
+++ idolpin/Database/AdPanelDBWorker.swift
@@ -0,0 +1,29 @@
+//
+//  AdPanelDBWorker.swift
+//  idolpin
+//
+//  Created by TaeinKim on 2020/01/25.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class AdPanelDBWorker {
+    func createAdPanel() {
+        
+    }
+    
+    func deleteAdPanel(id: Int) {
+        
+    }
+    
+    func retrieveAdPanelList(page: Int = 0) {
+        
+    }
+    
+    func updateAdPanel(id: Int) {
+        
+    }
+    
+}
