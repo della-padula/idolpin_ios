4ceb721d96f879dfded0c00d5b2b6aec0857ca2e
della-padula <della.kimko@gmail.com>
2020-01-08 21:10:17 +0900

    Filter View MVP 틀 구현

---
+++ idolpin/View/Filter/FilterContract.swift
@@ -0,0 +1,21 @@
+//
+//  FilterContract.swift
+//  idolpin
+//
+//  Created by TaeinKim on 2020/01/25.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import Foundation
+
+protocol FilterViewProtocol {
+    
+}
+
+protocol FilterPresenterProtocol {
+    
+}
+
+protocol FilterModelProtocol {
+    
+}
